982b350ab718cbf62f4769c3e0c3a7598da3a29d
Jan Frederik Hake <jan_hake@gmx.de>
2018-05-15 13:43:40 +0200

    removed state setting function calls from transfer_manager

--- database_scripts/core/transfer.sql
+++ database_scripts/core/transfer.sql
@@ -1,6 +1,40 @@
 SET search_path TO core,public;
 
 -- this is the entry point to the database
+
+-- request:
+-- {
+--     "action": "add",
+--     "schema": "core",
+--     "entity": "privilege",
+--     "data": {
+--         "name": "dasboard",
+--         "description": "show dashboard",
+--         "role_level": "guest"
+--     }
+-- }
+
+-- action: 
+-- * 'pending' -> initial value
+-- * 'processing', -> the coresponding manager does it's work
+-- * 'succeeded',
+-- * 'succeeded_with_warning',
+-- * 'error'
+
+-- entity:
+-- * `role`
+-- * `privilege`
+-- * `user_account`
+
+-- schema:
+-- * `core` -> the current schema for the operation
+
+-- data:
+-- * json object -> single result
+-- * json array -> multiple result
+-- * empty -> the data object is empty, when there was an error
+
+-- todo: what about error state?
 CREATE TABLE transfer(
     id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
     state transfer_state NOT NULL DEFAULT 'pending',
@@ -24,43 +58,42 @@ CREATE TRIGGER send_receipt_trigger BEFORE INSERT ON transfer
 -- only a state change will trigger a message
 CREATE TRIGGER got_response_trigger AFTER UPDATE ON transfer
     FOR EACH ROW 
-    WHEN (OLD.state IS DISTINCT FROM NEW.state)
+    WHEN (OLD.state IS DISTINCT FROM NEW.state AND NEW.state <> 'error')
         EXECUTE PROCEDURE send_transfer_message();
 
-
--- after a row is inserted into the `transfer` table
--- a message is send to the backend. 
--- the `transfer_manager` is called by the backend.
--- this approach should lead to fast inserts into the `transfer` table 
-
--- here comes the heavy lifting
+-- the `transfer_manager` chooses the corresponding entity manager for further processing
+-- if no exception is risen the backend sets the state to `succeeded`.
+-- if an exception is risen, the backend sets the state to `error`
 CREATE FUNCTION transfer_manager(transfer_id TEXT) RETURNS VOID AS $$
 DECLARE
     transfer_record RECORD;
     transfer_response JSONB;
 BEGIN
-    SELECT id, state, request, response FROM core.transfer WHERE id = transfer_id::UUID INTO transfer_record;
-    
-    --BEGIN
-        CASE transfer_record.request->>'entity'
-            WHEN 'role' THEN RAISE EXCEPTION 'role manager missing';
-            WHEN 'privilege' THEN 
-                BEGIN
-                    SELECT core.privilege_manager(transfer_record.request) INTO transfer_response;
-                    RAISE NOTICE 'privilege manager response: %', transfer_response;
-                    PERFORM core.set_response(transfer_id::UUID, transfer_response);
-                END;
-            WHEN 'user_account' THEN RAISE EXCEPTION 'user_account manager missing';
-            ELSE RAISE EXCEPTION 'entity `%` unknown', transfer_record.request->>'entity' 
-                USING HINT = 'entity must one of role, privilege or user_account';
-        END CASE;
-    --EXCEPTION
-    --    WHEN OTHERS THEN 
-    --        PERFORM set_error(transfer_id::UUID);
+   SELECT id, state, request, response FROM core.transfer WHERE id = transfer_id::UUID INTO transfer_record;
 
-    -- after the manager has succeeded the transfer record can be set to `succeed`
-    PERFORM core.set_succeeded(transfer_record.id);
-    RAISE NOTICE '% updated', transfer_record.request->>'entity';
+   CASE transfer_record.request->>'entity'
+       WHEN 'role' THEN 
+           BEGIN
+                RAISE EXCEPTION 'role manager missing for id %', transfer_record.id;
+           END;
+       WHEN 'privilege' THEN 
+           BEGIN
+               SELECT core.privilege_manager(transfer_record.request) INTO transfer_response;
+               RAISE NOTICE 'privilege manager response: %', transfer_response;
+               PERFORM core.set_response(transfer_id::UUID, transfer_response);
+           END;
+       WHEN 'user_account' THEN 
+           BEGIN 
+               RAISE EXCEPTION 'user_account manager missing';
+           END;
+       ELSE 
+           BEGIN
+               RAISE EXCEPTION 'entity `%` unknown', transfer_record.request->>'entity' 
+                   USING HINT = 'entity must one of role, privilege or user_account';
+           END;
+   END CASE;
+
+   RAISE NOTICE '% updated', transfer_record.request->>'entity';
 END
 $$ LANGUAGE plpgsql;
 
@@ -77,46 +110,33 @@ BEGIN
 END
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION set_pending(transfer_id UUID) RETURNS VOID AS $$
+CREATE FUNCTION set_pending(transfer_id TEXT) RETURNS VOID AS $$
 BEGIN
-    PERFORM core.set_state(transfer_id, 'pending');
+    PERFORM core.set_state(transfer_id::UUID, 'pending');
 END
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION set_processing(transfer_id UUID) RETURNS VOID AS $$
+CREATE FUNCTION set_processing(transfer_id TEXT) RETURNS VOID AS $$
 BEGIN
  
-    PERFORM core.set_state(transfer_id, 'processing'); 
+    PERFORM core.set_state(transfer_id::UUID, 'processing'); 
 END
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION set_succeeded(transfer_id UUID) RETURNS VOID AS $$
+CREATE FUNCTION set_succeeded(transfer_id TEXT) RETURNS VOID AS $$
 BEGIN
-    PERFORM core.set_state(transfer_id, 'succeeded'); 
+    PERFORM core.set_state(transfer_id::UUID, 'succeeded'); 
 END
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION set_succeded_with_warning(transfer_id UUID) RETURNS VOID AS $$
+CREATE FUNCTION set_succeded_with_warning(transfer_id TEXT) RETURNS VOID AS $$
 BEGIN
-    PERFORM core.set_state(transfer_id, 'succeeded_with_warning'); 
+    PERFORM core.set_state(transfer_id::UUID, 'succeeded_with_warning'); 
 END
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION set_error(transfer_id UUID) RETURNS VOID AS $$
+CREATE FUNCTION set_error(transfer_id TEXT) RETURNS VOID AS $$
 BEGIN
-    PERFORM core.set_state(transfer_id, 'error'); 
+    PERFORM core.set_state(transfer_id::UUID, 'error'); 
 END
 $$ LANGUAGE plpgsql;
-
-
--- after processing is finishesd handle notifications
--- based on the state
--- a worker function must not send notifications to the backend
-CREATE FUNCTION finished() RETURNS TRIGGER AS $$
-BEGIN
-    NEW.state = 'succeeded';
-    --todo: notify backend -> schema, entity, transfer id, status
-    RETURN NEW;
-END
-$$ LANGUAGE plpgsql;
-
