8fddb96d984d780d80fb804915c84b949f5eba57
Jan Frederik Hake <jan_hake@gmx.de>
2018-07-25 10:38:19 +0200

    added sample nodes to api_queries.sh
    The sample nodes describes some parts of rasmus.
    It is used as an example.

--- database_scripts/api_queries.sh
+++ database_scripts/api_queries.sh
@@ -22,7 +22,7 @@ curl -H "Content-Type: application/json" -d '{"action":"get","entity":"privilege
 curl -H "Content-Type: application/json" -d '{"action":"get","entity":"role"}' http://localhost:8080/api
 
 # wait two seconds, until the user is persisted. async operation. this is only for testing.
-sleep 2
+sleep 2 
 
 userid=`psql -U postgres -d rasmus -c "select id from rasmus.user" | sed -e '1,2d' -e '4,5d' -e 's/^ //'`
 admin_role_id=`psql -U postgres -d rasmus -c "select id from rasmus.role where name = 'admin'" | sed -e '1,2d' -e '4,5d' -e 's/^ //'`
@@ -31,5 +31,26 @@ curl -H "Content-Type: application/json" -d '{"action":"update","entity":"user",
 
 curl -H "Content-Type: application/json" -d '{"action":"get","entity":"user"}' http://localhost:8080/api
 
-curl -H "Content-Type: application/json" -d '{"action" : "add", "entity":"link", "data" : { "id_owner":"'"$userid"'", "name": "Google", "description" : "Search engine", "url" : "https://www.google.com" }}' http://localhost:8080/api
+# example links / nodes describing rasmus itself
 
+curl -H "Content-Type: application/json" -d '{"action" : "add", "entity":"link", "data" : { "id_owner":"'"$userid"'", "name": "otp tree", "description" : "process configuration", "url" : "https://github.com/enter-haken/rasmus/blob/master/lib/rasmus_app.ex" }}' http://localhost:8080/api
+
+curl -H "Content-Type: application/json" -d '{"action" : "add", "entity":"link", "data" : { "id_owner":"'"$userid"'", "name": "router", "description" : "cowboy router", "url" : "https://github.com/enter-haken/rasmus/blob/master/lib/web/router.ex" }}' http://localhost:8080/api
+
+curl -H "Content-Type: application/json" -d '{"action" : "add", "entity":"link", "data" : { "id_owner":"'"$userid"'", "name": "counter", "description" : "listen to notifications\nfrom database", "url" : "https://github.com/enter-haken/rasmus/blob/master/lib/core/counter.ex" }}' http://localhost:8080/api
+
+curl -H "Content-Type: application/json" -d '{"action" : "add", "entity":"link", "data" : { "id_owner":"'"$userid"'", "name": "inbound", "description" : "send requests towards\nthe database", "url" : "https://github.com/enter-haken/rasmus/blob/master/lib/core/inbound.ex" }}' http://localhost:8080/api
+
+curl -H "Content-Type: application/json" -d '{"action" : "add", "entity":"link", "data" : { "id_owner":"'"$userid"'", "name": "manager", "description" : "execute the\ndatabase manager", "url" : "https://github.com/enter-haken/rasmus/blob/master/lib/core/manager.ex" }}' http://localhost:8080/api
+
+curl -H "Content-Type: application/json" -d '{"action" : "add", "entity":"link", "data" : { "id_owner":"'"$userid"'", "name": "client", "description" : "react / visjs app", "url" : "https://github.com/enter-haken/rasmus/blob/master/lib/core/manager.ex" }}' http://localhost:8080/api
+
+curl -H "Content-Type: application/json" -d '{"action" : "add", "entity":"link", "data" : { "id_owner":"'"$userid"'", "name": "configuration", "description" : "database configuration", "url" : "https://github.com/enter-haken/rasmus/tree/master/config" }}' http://localhost:8080/api
+
+curl -H "Content-Type: application/json" -d '{"action" : "add", "entity":"link", "data" : { "id_owner":"'"$userid"'", "name": "database", "url" : "https://github.com/enter-haken/rasmus/tree/master/database_scripts" }}' http://localhost:8080/api
+
+curl -H "Content-Type: application/json" -d '{"action" : "add", "entity":"link", "data" : { "id_owner":"'"$userid"'", "name": "transfer", "description" : "interface table", "url" : "https://github.com/enter-haken/rasmus/blob/master/database_scripts/transfer.sql" }}' http://localhost:8080/api
+
+curl -H "Content-Type: application/json" -d '{"action" : "add", "entity":"link", "data" : { "id_owner":"'"$userid"'", "name": "postcreate", "description" : "table manipulation\nafter DDL", "url" : "https://github.com/enter-haken/rasmus/blob/master/database_scripts/postcreate.sql" }}' http://localhost:8080/api
+
+curl -H "Content-Type: application/json" -d '{"action" : "add", "entity":"link", "data" : { "id_owner":"'"$userid"'", "name": "crud", "description" : "generic CREATE, READ\nUPDATE, DELETE\nfunctions", "url" : ""https://github.com/enter-haken/rasmus/blob/master/database_scripts/crud.sql" }}' http://localhost:8080/api
